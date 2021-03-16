c9f07b960ce9473da5ee604c6b2ec4f7f6c7c476
Shivank Singhal <shivank.singhal.970@gmail.com>
2021-03-16 20:57:08 +0530

    added addTest

---
+++ src/test/java/com/example/demo/AddTest.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddTest {
+
+	@Autowired
+	CalculatorController controller;
+	
+	@Test
+	public void testAddSuccess() {
+		String a = "999";
+		String b = "10000";
+		
+		int result = controller.add(a, b);
+		assertEquals(10999, result);
+	}
+	
+	@Test
+	public void testAddWithWrongParameters() {
+		assertThrows(NumberFormatException.class, () -> {
+			String a = "100";
+			String b = "XX";
+			
+			controller.add(a, b);
+			controller.add(b, a);
+		});
+	}
+	
+	@Test
+	void testAddWithIntegerOverflow() {
+		String a = "2147483647";
+		String b = "2147483647";
+		
+		int result = controller.add(a, b);
+		
+		assertEquals(-2, result);
+	}
+
+}
